82ddcb5250882a9aee85725a7fa55518f586dad9
Jayden Tavares <jayden.tavares@hotmail.com>
2021-12-15 21:28:30 +1100

    Create Entity Highlight

--- src/entity/entity.gd
+++ src/entity/entity.gd
@@ -5,15 +5,20 @@ signal start_turn
 signal end_turn
 
 onready var _state_machine: StateMachine = get_node("StateMachine")
+onready var _highlight: Sprite = get_node("Highlight")
+onready var _highlight_animation_player: AnimationPlayer = get_node("Highlight/AnimationPlayer")
 
 
 func get_stat(stat: String):
-    return get_node("Stats/%s" % stat)
+	return get_node("Stats/%s" % stat)
 
 
 func _on_Entity_start_turn():
-    _state_machine.transition_to("Active")
+	_state_machine.transition_to("Active")
+	_highlight_animation_player.play("Float")
 
 
 func _on_Entity_end_turn():
-    _state_machine.transition_to("Idle")
+	_state_machine.transition_to("Idle")
+	_highlight_animation_player.stop()
+	_highlight.visible = false
--- src/entity/player/states/active.gd
+++ src/entity/player/states/active.gd
@@ -1,5 +1,8 @@
 extends State
 
+onready var _highlight_animation_player: AnimationPlayer = owner.get_node("Highlight/AnimationPlayer")
+
+
 func unhandled_input(event):
     # press space to end turn
     if event.is_action_pressed("ui_select"):
